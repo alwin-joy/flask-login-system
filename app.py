c61200777e2a9684030c4ef0aca3cb17b7f29502
alwin-joy <alwinj825@gmail.com>
2026-06-27 00:51:45 +0530

    update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True 
-app.config['MAIL_USERNAME'] = 'alwinj825@gmail.com'
-app.config['MAIL_PASSWORD'] = 'beof qdtl dtpj rxyo'
+app.config['MAIL_USERNAME'] = 'yourgmai@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your_app_password'
 
 # Initialize Database
 db = SQLAlchemy(app)
